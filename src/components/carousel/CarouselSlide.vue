3bdeae3b91701fc5fd5d465b3f7e7ca4a5898a10
Yevhen <shershen.yevhen@gmai>
2023-07-09 13:24:09 +0200

    first commit in SecondBranch

---
+++ src/components/carousel/CarouselSlide.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+// interface PropTypes {
+//   numberSlide: number
+// }
+const props = defineProps({
+  numberSlide: { type: Number, required: true }
+})
+</script>
+<template>
+  <div class="slide">
+    <a href=""
+      ><p>{{ props.numberSlide }}</p></a
+    >
+  </div>
+</template>
+<style>
+.slide {
+  border: solid;
+  width: 50px;
+  height: 50px;
+}
+</style>
